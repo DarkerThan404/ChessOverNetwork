a6ac90947649a28bbdfe5f3382bc6602a5e79da4
David Truhlář <truhlik24@gmail.com>
2022-05-04 19:25:38 +0200

    Basic PreValidation

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -4,15 +4,69 @@ import Piece.*;
 import Player.Player;
 import Square.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Game {
     private int size = 8;
     private Square[][] board= new Square[size][size];
+    private String[] input;
+    private Integer[] fromInput = new Integer[2];
+    private Integer[] toInput = new Integer[2];
+    private String horizontal = "abcdefgh";
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-    public Game(){}
-
-    public void run(){
+    public Game(){
         InitializeBoard();
         draw();
+    }
+
+    public void run(){
+        try {
+            while(true){
+                getInput();
+                if(isValidCoord()){
+                    System.out.println("Is valid coord");
+                    System.out.println("From: x:" + fromInput[0] + ", y:" + fromInput[1]);
+                    System.out.println("To: x:" + toInput[0] + ", y:" + toInput[1]);
+                } else {
+                    System.out.println("Is not valid coord");
+                }
+            }
+        }catch (IOException ex) {
+
+        }
+    }
+
+    private void getInput() throws IOException {
+        input = bufferedReader.readLine().split("\s+");
+    }
+
+    private boolean isValidCoord(){
+        Integer from;
+        Integer to;
+        if(input.length != 2){
+            System.out.println("Length incorrect");
+            return false;
+        } else if(input[0].length() != 2 || input[1].length() != 2){
+            System.out.println("Word are short or too long");
+            return false;
+        } else if(horizontal.indexOf(input[0].charAt(0)) == -1 || horizontal.indexOf(input[1].charAt(0)) == -1){
+            System.out.println("Could not find character");
+            return false;
+        } else {
+            from = Integer.parseInt(input[0].substring(1));
+            to = Integer.parseInt(input[1].substring(1));
+            if(from > 8 || from < 1 || to > 8 || to < 1){
+                return false;
+            }
+        }
+        fromInput[0] = horizontal.indexOf(input[0].charAt(0)) ;
+        fromInput[1] = from - 1;
+        toInput[0] = horizontal.indexOf(input[1].charAt(0)) ;
+        toInput[1] = to - 1;
+        return true;
     }
 
     private void InitializeBoard(){
@@ -52,11 +106,14 @@ public class Game {
     }
     private void draw(){
         for(int x = 0; x < size; x++){
+            System.out.print((8 - x  )% 9 + " ");
             for(int y = 0; y < size; y++){
                 board[x][y].getPiece().draw();
+                System.out.print(" ");
                 //System.out.println(board[x][y]);
             }
             System.out.println();
         }
+        System.out.println("  a  b c  d  e f  g  h");
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,22 +2,10 @@ import Game.*;
 
 public class Main {
     public static void main(String[] args){
-        int[][] lol = new int[8][8];
-        for(int i = 0; i < 8; i++){
-            lol[1][i] = 4;
-            lol[6][i] = 3;
-        }
-
-        for(int i = 0; i < 8; i++){
-            for(int j = 0; j < 8; j++){
-                System.out.print(lol[i][j] + " ");
-            }
-            System.out.println();
-        }
         Game game = new Game();
         game.run();
-        System.out.println("Hello world");
-        System.out.println("\u2657");
-        System.out.println("\u265D");
+        //System.out.println("Hello world");
+        //System.out.println("\u2657");
+        //System.out.println("\u265D");
     }
 }
