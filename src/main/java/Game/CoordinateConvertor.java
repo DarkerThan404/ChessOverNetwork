b5ba7e751463610cbbc9b1e25f476bd80bc6f0b3
David Truhlář <truhlik24@gmail.com>
2022-05-05 23:04:37 +0200

    Middle of work

--- src/main/java/Game/CoordinateConvertor.java
+++ src/main/java/Game/CoordinateConvertor.java
@@ -5,16 +5,16 @@ public class CoordinateConvertor {
     public static Integer[] StringToIntCoord(String pos){
         assert (pos.length() == 2);
         var result = new Integer[2];
-        result[0] = horizontal.indexOf(pos.charAt(0));
-        result[1] = Integer.parseInt(pos.substring(1)) - 1;
+        result[0] = horizontal.indexOf(pos.charAt(0)) ;
+        result[1] = (8 - Integer.parseInt(pos.substring(1)) % 9) ;
         return result;
     }
 
     public static String IntToStringCoord(Integer[] coord){
         assert (coord.length == 2);
         var result = "";
-        result += coord[0].toString();
-        result += coord[1].toString();
+        result += horizontal.charAt(coord[0]);
+        result += ((8 - coord[1]) % 9);
         return result;
     }
 }
